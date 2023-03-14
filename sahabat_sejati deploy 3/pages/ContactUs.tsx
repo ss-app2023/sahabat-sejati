2bd4b77dc2284a97242cd2fb35b6adda10b08848
ss-app2023 <127710274+ss-app2023@users.noreply.github.com>
2023-03-14 09:35:58 +0700

    Delete sahabat_sejati deploy 3 directory

--- sahabat_sejati deploy 3/pages/ContactUs.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const ContactUs = (props: Props) => {
-  return <div>ContactUs</div>;
-};
-
-export default ContactUs;
--- sahabat_sejati deploy 3/pages/jpnpages/HomeJpn.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const HomeJpn = (props: Props) => {
-  return <div>HomeJpn</div>;
-};
-
-export default HomeJpn;
